7293365d879a4e918cd28ff44bd9b0912eb98447
Jerome-eagle-OCR <jerome.rigault@expleogroup.com>
2021-04-12 21:57:15 +0200

    Class MainActivity - instantiated SharedPreferences to store player firstname and score - created method welcomeKnownPlayer to welcome back previous player if one (PREF_KEY_FIRSTNAME not null) - added call of welcomeKnownPlayer at the end of onActivityResult method

--- app/src/main/java/com/jr/topquiz/controller/MainActivity.java
+++ app/src/main/java/com/jr/topquiz/controller/MainActivity.java
@@ -1,10 +1,10 @@
 package com.jr.topquiz.controller;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -26,18 +26,29 @@ public class MainActivity extends AppCompatActivity {
     private Button mPlayButton;
     private User mUser;
     public static final int GAME_ACTIVITY_REQUEST_CODE = 33;
+    private SharedPreferences mPreferences;
+
+    public static final String PREF_KEY_SCORE = "PREF_KEY_SCORE";
+    public static final String PREF_KEY_FIRSTNAME = "PREF_KEY_FIRSTNAME";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mUser = new User();
+
+        mPreferences = getPreferences(MODE_PRIVATE);
+
         mGreetingText = findViewById(R.id.activity_main_greeting_txt);
         mNameInput = findViewById(R.id.activity_main_name_input);
         mPlayButton = findViewById(R.id.activity_main_play_btn);
-        mUser = new User();
 
         mPlayButton.setEnabled(false);
+
+        if (mPreferences.getString(PREF_KEY_FIRSTNAME, null) != null) {
+            welcomeKnownPlayer();
+        }
 
         mNameInput.addTextChangedListener(new TextWatcher() {
             @Override
@@ -56,20 +67,38 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        mPlayButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // User clicked let's play button
-                mUser.setFirstName(mNameInput.getText().toString());
-                Intent gameActivityIntent = new Intent(MainActivity.this, GameActivity.class);
-                startActivityForResult(gameActivityIntent, GAME_ACTIVITY_REQUEST_CODE);
-            }
+        mPlayButton.setOnClickListener(v -> {
+            // User clicked let's play button
+            mUser.setFirstName(mNameInput.getText().toString());
+
+            mPreferences.edit().putString(PREF_KEY_FIRSTNAME, mUser.getFirstName()).apply();
+
+            Intent gameActivityIntent = new Intent(MainActivity.this, GameActivity.class);
+            startActivityForResult(gameActivityIntent, GAME_ACTIVITY_REQUEST_CODE);
         });
     }
+
+    private void welcomeKnownPlayer() {
+        String firstname = mPreferences.getString(PREF_KEY_FIRSTNAME, null);
+        int score = mPreferences.getInt(PREF_KEY_SCORE, 0);
+        String welcomeBackText = "Bon retour dans le Top Quiz, " + firstname
+                + " !\nTon score précédent était " + score
+                + ".\nFeras-tu mieux cette fois ?";
+        mGreetingText.setText(welcomeBackText);
+        mNameInput.setText(firstname);
+        //mNameInput.setSelection(firstname.length());
+        mPlayButton.setEnabled(true);
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
         if (GAME_ACTIVITY_REQUEST_CODE == requestCode && RESULT_OK == resultCode) {
             int score = data.getIntExtra(GameActivity.BUNDLE_EXTRA_SCORE, 0);
+
+            mPreferences.edit().putInt(PREF_KEY_SCORE, score).apply();
+
+            welcomeKnownPlayer();
         }
     }
 }
